178a2cf68a66df3643c9aaedea6fdc31fa5567dd
Kosalvireak Khoeun <virakvary@gmail.com>
2022-06-17 17:14:12 +0700

    Add project files.

---
+++ Lesson_02_Example(Accessor)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_02_Example_Accessor_
+{
+    class House
+    {
+        public string strType;          //property
+        private string strAddress;
+        public string Address   //Address is the read & write accessor //It is an property
+        {
+            get { return strAddress; }
+            set { strAddress = value; }
+        }
+        private float fltPrice;
+        public float Price              //property
+        {
+            get { return fltPrice; }
+            set { fltPrice = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
