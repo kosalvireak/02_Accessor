2316b6413915a30612cbc076b8d9c67fa8d12f9f
Kosalvireak Khoeun <virakvary@gmail.com>
2022-06-21 17:27:48 +0700

    Finish Lesson02Ex3

---
+++ Lesson02Ex3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02Ex3
+{
+    class Circle
+    {
+        private float fltRadius;
+        public float Radius
+        {
+            get { return fltRadius; }
+            set { if (value > 0)fltRadius = value; }
+        }
+        private string strColor;
+        public string Color
+        {
+            get { return strColor; }
+            set { strColor = value; }
+        }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Circle cObj = new Circle();
+                Console.Write("Enter the radius: ");
+                cObj.Radius = float.Parse(Console.ReadLine());
+
+                Console.Write("Enter the color: ");
+                cObj.Color = (Console.ReadLine());
+
+                Console.WriteLine("The radius: {0}, The color: {1} ",cObj.Radius,cObj.Color);
+
+        }
+    }
+    }
+}
